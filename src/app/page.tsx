147574bc2d3c8eb44dd2389405c4773fd67dacf9
Holy <oleg.chikolovec@gmail.com>
2024-08-15 18:48:39 +0300

    updatwe

---
+++ src/app/page.tsx
@@ -0,0 +1,123 @@
+import { Container, Filters, Title } from "./shared";
+import { ProductCard } from "./shared/product-card";
+import { ProductsGroupList } from "./shared/products-group-list";
+import { TopBar } from "./shared/top-bar";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товаров */}
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Пиццы"
+                categoryId={1}
+                products={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+              <ProductsGroupList
+                title="Комбо"
+                categoryId={2}
+                products={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:233x233/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
---
+++ src/app/shared/products-group-list.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { FC, useEffect, useRef } from "react";
+import { Title } from "./title";
+import { cn } from "@/lib/utils";
+import { ProductCard } from "./product-card";
+import { useIntersection } from "react-use";
+import { useCategoryStore } from "@/store/category";
+
+type Props = {
+  title: string;
+  products: any["products"];
+  className?: string;
+  listClassName?: string;
+  categoryId: number;
+};
+
+export const ProductsGroupList: FC<Props> = ({
+  title,
+  products,
+  className,
+  listClassName,
+  categoryId,
+}) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+
+  return (
+    <div className={className} id={title} ref={intersectionRef}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+
+      <div className={cn("grid grid-cols-3 gap-[80px]", listClassName)}>
+        {products.map((product: any) => {
+          return (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              imageUrl={product.imageUrl}
+              price={product.items[0].price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
